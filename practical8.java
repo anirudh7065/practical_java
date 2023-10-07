4c5e35d4ad4ba6fdc52ab0929f66557c730ca3ee
anirudh7065 <107138932+anirudh7065@users.noreply.github.com>
2023-10-07 22:01:23 +0530

    Initial commit

--- practical8.java
+++ practical8.java
@@ -1,6 +1,29 @@
-public class practical8 {
-    public static void main(String[] args){
-        int a = 5;
-        System.out.println(a*a);
+public class practical8 extends Thread{
+    private final int thread1;
+    public practical8(int a){
+        thread1 = a;
     }
+    @Override
+    public void run() {
+            for (int i = 0 ; i <= 5 ; i++) {
+                System.out.println(i + "this is from "+ thread1);
+            try {
+                    Thread.sleep(1000);
+                }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+    }
+
+    public static void main(String[] args){
+        System.out.println("Program of implementing multithreading by extending class");
+        for (int i = 0  ; i <= 5 ; i++) {
+            practical8 t1 = new practical8(i);
+            t1.start();
+        }
+
+
+    }
+
 }
