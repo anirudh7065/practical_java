2c0a6bbb853d282199d4a2dd3aa92bf05495de4e
anirudh7065 <anirudhraj02@gmail.com>
2023-11-24 01:29:53 +0530

    Initial commit

--- practical22.java
+++ practical22.java
@@ -1,2 +1,26 @@
+abstract class animal{
+    abstract void sound();
+}
+
+class Lion extends animal{
+    @Override
+    public void sound(){
+        System.out.println("Roar...Roar..!!");
+    }
+}
+class Tiger extends animal{
+    @Override
+    public void sound(){
+        System.out.println("Grr..Grr..!!");
+    }
+}
 public class practical22 {
+//    Write a Java program to create an abstract class Animal with an abstract method called sound(). Create subclasses Lion and Tiger that extend the Animal class and implement the sound() method to make a specific sound for each animal.
+
+    public static void main(String[] args){
+        Lion a = new Lion();
+        Tiger b = new Tiger();
+        a.sound();
+        b.sound();
+    }
 }
